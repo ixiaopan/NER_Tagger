6a0895b615b4644f53fe72e4d925d9e329a9fea5
xiaopan <xiaopan.wpp@outlook.com>
2021-07-23 17:11:16 +0100

    fix:import torch"

--- batch_train_pool_init.py
+++ batch_train_pool_init.py
@@ -1,7 +1,8 @@
 import os
 from shutil import copy
-
+import torch
 from utils import utils
+
 from batch_train_baseline import train_and_evaluate
 from models.BiLSTM_batch import BiLSTM_CRF_Batch
 
