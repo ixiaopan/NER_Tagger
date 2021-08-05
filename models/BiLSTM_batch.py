d496a350634b1055a51087e9fe739f1748ad978f
xiaopan <xiaopan.wpp@outlook.com>
2021-08-05 19:39:16 +0100

    char

--- models/BiLSTM_batch.py
+++ models/BiLSTM_batch.py
@@ -363,10 +363,11 @@ class BiLSTM_CRF_Batch(nn.Module):
 
       # (batch_size, max_seq_len, char_hidden_dim)
       max_seq_len = embed_words.shape[1]
-      concat_char_embed_expand = torch.zeros(( embed_words.shape[0], max_seq_len, concat_char_embed.shape[1] ), dtype=torch.float) # for padding word, use zero vector
+       # for padding word, use zero vector
+      concat_char_embed_expand = torch.zeros(( embed_words.shape[0], max_seq_len, concat_char_embed.shape[1] ), dtype=torch.float).to(self.device)
       prev_seq_len = 0
       for i, seq_len in enumerate(seq_len_in_batch): # each sentence
-        flat_char_embedding = torch.zeros(( max_seq_len, concat_char_embed.shape[1] ), dtype=torch.float)
+        flat_char_embedding = torch.zeros(( max_seq_len, concat_char_embed.shape[1] ), dtype=torch.float).to(self.device)
 
         flat_char_embedding[:seq_len, :] = concat_char_embed[prev_seq_len:prev_seq_len+seq_len]
 
